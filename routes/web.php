74ecb25915a29abbf9285dc0639d516ab2b35335
DAliRIJTK <148074211+DAliRIJTK@users.noreply.github.com>
2024-12-08 11:02:38 +0700

    laravel breeze Install dan config + role
    -composer install
    -composer require laravel/breeze --dev
    -php artisan breeze:install
    -npm install
    -npm install vite
    -npm install nodejs
    -npm run build
    -npm run dev
    -php artisan migrate
    -php artisan key:generate
    -php artisan serve

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('admin/create-kegiatan', [App\Http\Controllers\PageController::class, 'createKegiatan'])->name('create-kegiatan');
-Route::get('admin/list-kegiatan', [App\Http\Controllers\PageController::class, 'listKegiatan'])->name('list-kegiatan');
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin/create-kegiatan', [App\Http\Controllers\PageController::class, 'createKegiatan'])->name('create-kegiatan');
+Route::get('/admin/list-kegiatan', [App\Http\Controllers\PageController::class, 'listKegiatan'])->name('list-kegiatan');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
