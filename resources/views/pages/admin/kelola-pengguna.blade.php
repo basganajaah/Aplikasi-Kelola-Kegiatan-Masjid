e3a434b44ed800e5b53994567a22ec0953f26247
basganajaah <bandyagaadiansyah@gmail.com>
2024-12-08 10:07:01 +0700

    add buttons pages/admin/kelola-pengguna

--- resources/views/pages/admin/kelola-pengguna.blade.php
+++ resources/views/pages/admin/kelola-pengguna.blade.php
@@ -6,56 +6,56 @@
 
 @section('content')
     @section('plugins.Datatables', true)
-    {{-- Setup data for datatables --}}
-    @php
-    $heads = [
-        'ID',
-        'Name',
-        ['label' => 'Phone', 'width' => 40],
-        ['label' => 'Actions', 'no-export' => true, 'width' => 5],
-    ];
+        @section('plugins.DatatablesPlugins', true)
+            @php
+            $heads = [
+                'ID',
+                'Name',
+                ['label' => 'Phone', 'width' => 40],
+                ['label' => 'Actions', 'no-export' => true, 'width' => 5],
+            ];
 
-    $btnEdit = '<button class="btn btn-xs btn-default text-primary mx-1 shadow" title="Edit">
-                    <i class="fa fa-lg fa-fw fa-pen"></i>
-                </button>';
-    $btnDelete = '<button class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
-                    <i class="fa fa-lg fa-fw fa-trash"></i>
-                </button>';
-    $btnDetails = '<button class="btn btn-xs btn-default text-teal mx-1 shadow" title="Details">
-                    <i class="fa fa-lg fa-fw fa-eye"></i>
-                </button>';
+            $btnEdit = '<button class="btn btn-xs btn-default text-primary mx-1 shadow" title="Edit">
+                            <i class="fa fa-lg fa-fw fa-pen"></i>
+                        </button>';
+            $btnDelete = '<button class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
+                            <i class="fa fa-lg fa-fw fa-trash"></i>
+                        </button>';
+            $btnDetails = '<button class="btn btn-xs btn-default text-teal mx-1 shadow" title="Details">
+                            <i class="fa fa-lg fa-fw fa-eye"></i>
+                        </button>';
 
-    $config = [
-        'data' => [
-            [1, 'John Bender', '+02 (123) 123456789', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [2, 'Sophia Clemens', '+99 (987) 987654321', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [3, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [4, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [5, 'Peter asd', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [6, 'Peter qwsdasd', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [7, 'Peter Adasd', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [8, 'Peter Sasfasousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [9, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [10, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [11, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [12, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-        ],
-        'order' => [[1, 'asc']],
-        'columns' => [null, null, null, ['orderable' => false]],
-    ];
-    @endphp
+            $config = [
+                'data' => [
+                    [1, 'John Bender', '+02 (123) 123456789', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [2, 'Sophia Clemens', '+99 (987) 987654321', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [3, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [4, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [5, 'Peter asd', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [6, 'Peter qwsdasd', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [7, 'Peter Adasd', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [8, 'Peter Sasfasousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [9, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [10, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [11, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                    [12, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
+                ],
+                'order' => [[1, 'asc']],
+                'columns' => [null, null, null, ['orderable' => false]],
+            ];
+            @endphp
 
-    <x-adminlte-datatable id="table1" :heads="$heads" head-theme="dark">
-        @foreach($config['data'] as $row)
-            <tr>
-                @foreach($row as $cell)
-                    <td>{!! $cell !!}</td>
+            <x-adminlte-datatable id="table7" :heads="$heads" head-theme="dark" footer-theme="dark" with-footer with-buttons hoverable>
+                @foreach($config['data'] as $row)
+                    <tr>
+                        @foreach($row as $cell)
+                            <td>{!! $cell !!}</td>
+                        @endforeach
+                    </tr>
                 @endforeach
-            </tr>
-        @endforeach
-    </x-adminlte-datatable>
-    
+            </x-adminlte-datatable>
 @endsection
+
 
 @section('footer')
     @include('components.footer')
